b45957f3bb23501b1447cf93610b1cf9a6d13264
Stephane <apsarawebdev@protonmail.com>
2021-02-19 11:18:25 +0700

    homepage ok

--- vues/frontend/baromaire_home/home.php
+++ vues/frontend/baromaire_home/home.php
@@ -1,9 +1,25 @@
 <?php 
 /**
-* Page : Créer Votre Baromaire
+* Page : Homepage
 */ 
 // content
 ob_start(); ?>
-HOME
+
+<div class="container text-center mb-5"><br>
+    <!-- title -->
+    <h1 class="mb-5">Votre Maire respecte-t-il ses engagements ?</h1>
+    <!-- subtitle -->
+    <h5 class="primary mb-4">Tapez le nom de votre commune ou son code postal</h5>
+    <!-- form -->
+    <form class="form-inline justify-content-center">
+        <div class="form-group mb-2 mr-3 col-4 pl-0 pr-0">
+            <input type="text" class="form-control col-12" id="commune" value="75015 ou Paris">
+            <!-- error message -->
+            <div class="invalid-feedback">Commune ou code postal inconnus</div>
+        </div>
+        <button type="submit" class="btn btn-primary mb-2">Envoyer</button>
+    </form>
+</div>
+
 <?php $content = ob_get_clean(); ?>
 <?php require('vues/layout_home.php'); ?>
--- vues/layout_home.php
+++ vues/layout_home.php
@@ -15,7 +15,7 @@
     <!-- custom style -->
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
-<body id="page-top">
+<body id="position-relative" style="min-height:100vh">
     <div class="container-baromaire mb-5">
         <!-- header -->
         <header class="position-relative header_baromaire row align-items-center justify-content-center pt-5 pb-5">
@@ -31,16 +31,16 @@
             </div>
         </header>
     </div>
-    <?php $content ?>
-    <footer class="text-white pb-2 pt-4">
+    <?= $content ?>
+    <footer class="text-white pb-1 pt-3 position-absolute" style="bottom:0;">
         <div class="f-flex flex-column">
             <nav class="d-flex justify-content-center">
-                <ul class="footer-nav pl-0 mb-3 d-flex font-22">
-                    <li><a href="" class="white<?php if ($_SERVER['REQUEST_URI']== "/"){ ?> active <?php } ?>">Principes</a></li><span class="ml-3 mr-3">/</span>
-                    <li><a href="" class="white">Démo</a></li><span class="ml-3 mr-3">/</span>
-                    <li><a href="" class="white">Créer votre Baromaire</a></li><span class="ml-3 mr-3">/</span>
-                    <li><a href="" class="white">Tarifs</a></li><span class="ml-3 mr-3">/</span>
-                    <li><a href="" class="white">Nous contacter</a></li>
+                <ul class="footer-nav pl-0 mb-2 d-flex font-22">
+                    <li><a href="" class="white<?php if ($_SERVER['REQUEST_URI']== "/"){ ?> active <?php } ?>">Principes</a></li><span class="ml-4 mr-4">/</span>
+                    <li><a href="" class="white<?php if (isset($_GET['demo'])){ ?> active <?php } ?>">Démo</a></li><span class="ml-4 mr-4">/</span>
+                    <li><a href="" class="white<?php if (isset($_GET['create'])){ ?> active <?php } ?>">Créer votre Baromaire</a></li><span class="ml-4 mr-4">/</span>
+                    <li><a href="" class="white<?php if (isset($_GET['tarifs'])){ ?> active <?php } ?>">Tarifs</a></li><span class="ml-4 mr-4">/</span>
+                    <li><a href="" class="white<?php if (isset($_GET['contact'])){ ?> active <?php } ?>">Nous contacter</a></li>
                 </ul>
             </nav>
             <div class="text-center font-12">Tous Droits Réservés Baromaire<sup style="font-size:7px"><i class="far fa-copyright text-white"></i></sup> 2021</div>
