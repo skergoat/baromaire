49ef5feffb30c07e9cb4b09a8e550e7a4cd6707f
Stephane <apsarawebdev@protonmail.com>
2021-02-19 17:52:55 +0700

    demarche ok

--- vues/frontend/baromaire_notation/demarche.php
+++ vues/frontend/baromaire_notation/demarche.php
@@ -14,11 +14,11 @@ ob_start(); ?>
 <!-- row 1 -->
 <div class="mb-4">
     <h4 class="mb-4">Notre démarche</h4>
-    <div class="row justify-content-sm-column">
-        <div class="col-6 p-0">
+    <div class="row flex-sm-column flex-lg-row">
+        <div class="col-sm-12 col-lg-6 p-0 mb-4 mb-lg-0">
             <img src="assets/img/engagements/image_29.jpg" alt="Team">
         </div>
-        <div class="col-6 pr-0">
+        <div class="col-sm-12 col-lg-6 pl-0 pl-lg-3 pr-0">
             <p class="primary font-16 text-justify mb-2" style="line-height:1.7rem">Vous le savez, à Beaubourg, nous innovons sans cesse pour transformer notre ville et la moderniser. Cela ne se limite pas uniquement à des projets ! Aujourd'hui, grace au Baromaire, c'est une manière plus transparente de vous informer que je vous propose. </p>
             <p class="primary font-16 text-justify mb-2" style="line-height:1.7rem">En visitant régulièrement notre Baromaire, vous pourrez suivre en temps réel la mise en oeuvre de tous les engagements que nous avons pris envers vous pour ces 6 prochaines années. Vous pourrez ainsi, pour chaque projet structurant de notre ville, suivre l'avancée des travaux et juger de la performance de votre équipe municipale.</p>
             <div class="row">
