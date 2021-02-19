4ae10948982996fe781759903bd660a70d2cb0ac
Stephane <apsarawebdev@protonmail.com>
2021-02-19 13:54:25 +0700

    font footer

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,5 @@
-
 $('#hamburger-button').on('click', function() {
     let height = $('#footer-home').height();
-    $('.footer-responsive-nav').css('bottom', (height + 20)+'px');
+    $('.footer-responsive-nav').css('bottom', (height + 0)+'px');
     $('.footer-responsive-nav').toggleClass('d-none');
 });
