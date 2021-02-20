bd2e6e4bbfa9c06398b3cd79e71b6bb2d264b914
Stephane <apsarawebdev@protonmail.com>
2021-02-20 10:02:07 +0700

    all responsive. reste layout widget

--- vues/backend/engagement.php
+++ vues/backend/engagement.php
@@ -31,29 +31,29 @@ ob_start(); ?>
         <!-- body -->
         <div class="card-body pt-4 pb-4">
         <!-- form -->
-            <form>
+            <form id="form-sign">
                 <div class="row">
-                    <div class="col-12 mb-3">
+                    <div class="col-12 p-0 mb-3">
                         <!-- row 1 -->
                         <div class="row">
                             <!-- order -->
-                            <div class="form-group col-2 pl-0">
+                            <div class="form-group col-lg-2 pl-0">
                                 <label for="order" class="w-900">Ordre</label>
                                 <input type="number" class="form-control <?php if(isset($_GET['reload'])) { ?> is-invalid  <?php } ?>" min="0" max="10" id="order" placeholder="1">
                                 <div class="invalid-feedback">Chiffre invalide</div>
                             </div>
-                            <div class="form-group col-2 pl-0">
+                            <div class="form-group col-lg-2 pl-0">
                                 <!-- realisation -->
                                 <label for="realisation" class="w-900">Réalisation</label>
                                 <div class="input-group">
-                                    <input type="text" class="form-control" placeholder="0" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                                    <input type="text" class="form-control" id="input-percent" placeholder="0" aria-label="Recipient's username" aria-describedby="basic-addon2">
                                     <div class="input-group-append">
                                         <span class="input-group-text" id="basic-addon2">%</span>
                                     </div>
                                 </div>
                             </div>
                             <!-- engagement -->
-                            <div class="form-group col-8 pl-0 pr-0">
+                            <div class="form-group col-lg-8 pl-0 pr-0">
                                 <label for="engagement" class="w-900">Engagement</label>
                                 <input type="text" class="form-control <?php if(isset($_GET['reload'])) { ?> is-invalid  <?php } ?>" id="engagement" placeholder="Rénover les arrêts de bus, etc.">
                                 <div class="invalid-feedback">
@@ -63,10 +63,10 @@ ob_start(); ?>
                         </div>
                     </div> 
                     <!-- row 2 -->
-                    <div class="col-12 mb-3">
+                    <div class="col-12 p-0 mb-3">
                         <div class="row">
                             <!-- photo -->
-                            <div class="form-group col-4 pl-0">
+                            <div class="form-group col-lg-4 pl-0">
                                 <label for="order" class="w-900">Photo</label>
                                 <div class="input-group">
                                     <div class="custom-file">
@@ -78,7 +78,7 @@ ob_start(); ?>
                                 </div>
                             </div>
                             <!-- domaine -->
-                            <div class="form-group col-4 pl-0 pr-0">
+                            <div class="form-group col-lg-4 pl-0 pr-0">
                                 <label for="exampleFormControlSelect1" class="w-900">Domaine d'action</label>
                                 <select class="form-control" id="exampleFormControlSelect1">
                                     <?php foreach($cards->create() as $domaine) { ?>
@@ -87,7 +87,7 @@ ob_start(); ?>
                                 </select>
                             </div>
                             <!-- quartier -->
-                            <div class="form-group col-4 pr-0">
+                            <div class="form-group col-lg-4 pr-0">
                                 <label for="exampleFormControlSelect1" class="w-900">Quartier</label>
                                 <select class="form-control" id="exampleFormControlSelect1">
                                     <?php foreach($cards->quartier() as $quartier) { ?>
@@ -99,7 +99,7 @@ ob_start(); ?>
                     </div>     
                 </div>
                 <!-- textarea -->
-                <div class="col-12 mb-4">
+                <div class="col-12 p-0 mb-4">
                     <div class="form-group">
                         <label for="exampleFormControlTextarea1" class="w-900">Description</label>
                         <textarea class="form-control <?php if(isset($_GET['reload'])) { ?> is-invalid  <?php } ?>" id="exampleFormControlTextarea1" rows="5"></textarea>
@@ -108,11 +108,11 @@ ob_start(); ?>
                         </div>
                     </div>
                 </div>
-                <div class="col-12">
-                    <div class="row justify-content-between">
-                        <button class="btn btn-primary" id="submit-1"><i class="fas fa-redo-alt mr-1"></i> Enregistrer et ajouter un engagement</button>
-                        <button class="btn btn-primary" id="submit-2"><i class="fas fa-plus mr-1"></i> Ajouter une action</button>
-                        <button class="btn btn-primary" id="submit-3"><i class="fas fa-directions mr-1"></i> Enregistrer et quitter</button>
+                <div class="col-12 p-0">
+                    <div class="row justify-content-lg-center align-items-sm-center">
+                        <button class="btn btn-primary col-md-7 mb-md-1" id="submit-1"><i class="fas fa-redo-alt mr-1"></i> Enregistrer et ajouter un engagement</button>
+                        <button class="btn btn-primary col-md-7 mb-md-1" id="submit-2"><i class="fas fa-plus mr-1"></i> Ajouter une action</button>
+                        <button class="btn btn-primary col-md-7 mb-md-1" id="submit-3"><i class="fas fa-directions mr-1"></i> Enregistrer et quitter</button>
                     </div>
                 </div>
             </form>
