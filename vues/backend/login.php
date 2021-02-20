3ca745bf875ffdb7b58ef31becd2a6c7fdc77b4a
Stephane <apsarawebdev@protonmail.com>
2021-02-20 09:19:06 +0700

    login page responsive ok

--- vues/backend/login.php
+++ vues/backend/login.php
@@ -13,13 +13,13 @@ ob_start(); ?>
 
 <div class="row">
 <!-- card top -->
-  <div class="col-sm-12 mb-5 login-card-top">
+  <div class="col-lg-12 mb-5 login-card-top">
     <div class="card">
       <div class="card-body">
         <!-- title -->
         <div class="row align-items-center">
             <img src="assets/img/admin/image_28.jpg" alt="" class="col-2 pr-0" id="login-logo-1"> 
-            <p class="col-sm-10 card-title danger font-25 text-center w-900 pr-0 pl-0">Pas de Baromaire dans votre commune ?</p>
+            <p class="col-lg-10 card-title danger font-25 text-center w-900 pr-0 pl-0">Pas de Baromaire dans votre commune ?</p>
         </div>
         <!-- body -->
         <p class="card-title text-center font-23">Vous pouvez engager votre commune ?</p>
@@ -30,25 +30,31 @@ ob_start(); ?>
     </div>
   </div>
   <!-- card bottom -->
-  <div class="col-sm-12 login-card-bottom">
+  <div class="col-lg-12 login-card-bottom">
     <div class="card">
       <div class="card-body">
+        <p class="card-title text-center font-23 d-none" id="login-logo-p-3">
+          <img src="assets/img/icone_m.png" alt="icon baromaire" class="pr-0" id="login-logo-3">
+        </p>
         <!-- title -->
-        <p class="card-title text-center font-23"><img src="assets/img/icone_m.png" alt="icon baromaire" class="pr-0" id="login-logo-2">Acces à la gestion de votre Baromaire</p>
+        <p class="card-title text-center font-23">
+          <img src="assets/img/icone_m.png" alt="icon baromaire" class="pr-0 d-inline-block" id="login-logo-2">
+          Accès à la gestion de votre Baromaire
+        </p>
         <div class="alert alert-danger text-center mx-auto" id="alert-remove" role="alert" style="max-width:500px;">
           Email ou Mot de Passe incorrects 
         </div>
         <!-- form -->
         <form class="mb-3">
-            <div class="form-group row">
-                <label for="staticEmail" class="col-sm-3 col-form-label text-right">Email</label>
-                <div class="col-sm-9">
+            <div class="form-group row justify-content-center">
+                <label for="staticEmail" class="pl-0 col-lg-2 col-form-label text-right">Email</label>
+                <div class="col-lg-9 pr-0">
                     <input type="email" class="form-control" id="imputEmail" placeholder="email@example.com">
                 </div>
             </div><br>
-            <div class="row">
-                <label for="inputPassword2" class="col-sm-3 col-form-label text-right mb-0">Password</label>
-                <div class="form-group col-8 mb-0"> 
+            <div class="row justify-content-center">
+                <label for="inputPassword2" class=" pl-0 col-lg-2 col-form-label text-right mb-0">Password</label>
+                <div class="form-group col-lg-8 mb-0"> 
                     <input type="password" class="form-control" id="inputPassword2" style="width:100%;">
                 </div>
                 <button type="submit" class="btn btn-primary" id="submit">OK</button>
