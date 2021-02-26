a5ab60267f65ea1843be2c3e856a88d8a4dde8b9
Stephane <apsarawebdev@protonmail.com>
2021-02-27 05:45:53 +0700

    adfadfad

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ else if(isset($_GET['tarifs'])) {
     require('vues/frontend/baromaire_home/tarifs.php');
 }
 else if(isset($_GET['create_baromaire'])) {
-    require('vues/frontend/baromaire_home/create_baromaire.php');
+    require('vues/frontend/baromaire_home/create.php');
 }
 else if(isset($_GET['progres'])) {
     require('vues/frontend/baromaire_notation/progres.php');
