874f4ea20d6791e430dd7677c474f4faa9c80519
Stephane <apsarawebdev@protonmail.com>
2021-02-27 09:43:53 +0700

    photo

--- vues/layout_notation.php
+++ vues/layout_notation.php
@@ -44,9 +44,9 @@
                         <?php } ?>
                         </a>
                         <!-- img mayor -->
-                        <!-- <div class="image image-rounded img-thumbnail mx-auto mb-3"></div> -->
+                        <div class="image image-rounded img-thumbnail mx-auto mb-3"></div>
                         <!-- img placeholder -->
-                        <div class="image image-placeholder img-thumbnail mx-auto mb-3"></div>
+                        <!-- <div class="image image-placeholder img-thumbnail mx-auto mb-3"></div> -->
                     </div>
                     <div class="row">
                         <div class="col-12 p-0">
