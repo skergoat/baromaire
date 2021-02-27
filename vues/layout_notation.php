639b9e834445819301cd6795cbb673331c7a778e
Stephane <apsarawebdev@protonmail.com>
2021-02-27 08:33:37 +0700

    12

--- vues/layout_notation.php
+++ vues/layout_notation.php
@@ -65,7 +65,7 @@
                         <?php } ?>
                     </div>
                 </div>
-                <ul class="col-6 mx-auto text-left">
+                <ul class="col-12 col-md-6 mx-auto text-left">
                     <li class="pl-2"><a href="/?progres"><i class="fas fa-ruler-combined mr-3"></i>Suivre les progrès</a></li>
                     <li class="pl-2"><a href="/?demarche"><i class="fas fa-puzzle-piece mr-3"></i>Notre démarche</a></li>
                     <li class="pl-2"><a href="/?engagement"><i class="fas fa-file-signature mr-3"></i>Nos engagements</a></li>
