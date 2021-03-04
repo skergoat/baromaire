3283d9937b9f6d545c2c003d73d0b607ab338b0f
Stephane <apsarawebdev@protonmail.com>
2021-03-04 09:46:54 +0700

    auto

--- vues/frontend/baromaire_home/home.php
+++ vues/frontend/baromaire_home/home.php
@@ -11,13 +11,22 @@ ob_start(); ?>
         <!-- subtitle -->
         <h5 class="main-subtitle primary mb-sm-4">Tapez le nom de votre commune ou son code postal</h5>
         <!-- form -->
-        <form class="form-inline justify-content-center">
-            <div class="form-group mb-sm-2 mr-sm-3 col-4 pl-0 pr-0">
-                <input type="text" class="form-control col-sm-12" id="commune" placeholder="ex. : 75015 ou Paris" autocomplete>
-                <!-- error message -->
-                <div class="invalid-feedback">Commune ou code postal inconnus</div>
+        <form>
+            <div class="form-inline justify-content-center">
+                <div class="form-group position-relative mb-sm-2 mr-sm-3 col-4 pl-0 pr-0">
+                    <input type="text" class="form-control col-sm-12" id="commune" placeholder="ex. : 75015 ou Paris" autocomplete>
+                    <!-- error message -->
+                    <div class="invalid-feedback">Commune ou code postal inconnus</div>
+                    <ul class="p-0 col-12 position-absolute bg-white text-left black" style="list-style-type:none;text-align:center;top:35px;border:1px solid #ced4da">
+                        <li class="pl-3">Paris</li>
+                        <li class="pl-3">Parnasse</li>
+                        <li class="pl-3">Paramount</li>
+                        <li class="pl-3">Paraphine</li>
+                        <li class="pl-3">Partout</li>
+                    </ul>
+                </div>
+                <button type="submit" class="submit btn btn-primary mb-2">Envoyer</button>
             </div>
-            <button type="submit" class="submit btn btn-primary mb-2">Envoyer</button>
         </form>
     </div>
 <!-- redirect provisoire -->
