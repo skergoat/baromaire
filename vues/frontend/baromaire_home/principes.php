80a6ff76ed92dbfa0cee150fd20945b76b563581
Stephane <apsarawebdev@protonmail.com>
2021-03-04 09:21:16 +0700

    xl

--- vues/frontend/baromaire_home/principes.php
+++ vues/frontend/baromaire_home/principes.php
@@ -9,14 +9,14 @@ ob_start(); ?>
         <div class="col-12 col-lg-6 pl-0 pr-0 mb-5 mb-lg-0">
             <div class="row">
                 <div class="col-12 col-md-6 mb-5 mb-md-0 pr-0 pr-lg-3 pl-0">
-                    <div class="card card-principes shadow">
+                    <div class="card card-principes principes shadow">
                         <div class="card-body pb-5 pt-4">
                             <div class="pt-3 mb-4">
-                                <img src="assets/img/principes/principes-3.jpg" alt="" style="max-width:150px;"/>
+                                <img src="assets/img/principes/principes-3.jpg" alt=""/>
                             </div>
                             <div>
                                 <div class="w-900 font-25 mb-3">LE BAROMAIRE</div>
-                                <div class="content">
+                                <div class="principes content">
                                     est un outil permettant de presenter 
                                     les engagements du Maire pour la realisation 
                                     de projets concrets et mesurables prandant
@@ -27,14 +27,14 @@ ob_start(); ?>
                     </div>
                 </div>
                 <div class="col-12 col-md-6 pr-0 pl-0 pr-lg-3 pl-lg-3">
-                    <div class="card card-principes shadow">
+                    <div class="card card-principes principes shadow">
                         <div class="card-body pb-2 pt-4" style="min-height:100%;">
                             <div class="pt-3 mb-4">
-                                <img src="assets/img/principes/principes-2.jpg" alt="" style="max-width:150px;"/>
+                                <img src="assets/img/principes/principes-2.jpg" alt=""/>
                             </div>
                             <div>
                                 <div class="w-900 font-25 mb-3">LE BAROMAIRE</div>
-                                <div class="content">
+                                <div class="principes content">
                                     classe les projets par thématique et par localisation,
                                     permettant ainsi aux administrés d'accéder à une vue 
                                     synthétique du progrès réalisé pendant la durée du mandat.
@@ -48,14 +48,14 @@ ob_start(); ?>
         <div class="col-12 col-lg-6 pl-0 pr-0">
             <div class="row">
                 <div class="col-12 col-md-6 mb-5 mb-md-0 pr-0 pl-0 pr-lg-3 pl-lg-3">
-                    <div class="card card-principes shadow">
+                    <div class="card card-principes principes shadow">
                         <div class="card-body pb-5 pt-4">
                             <div class="pt-3 mb-4">
-                                <img src="assets/img/principes/principes-1.jpg" alt="" style="max-width:150px;"/>
+                                <img src="assets/img/principes/principes-1.jpg" alt=""/>
                             </div>
                             <div>
                                 <div class="w-900 font-25 mb-3">LE BAROMAIRE</div>
-                                <div class="content">
+                                <div class="principes content">
                                     affiche l'état d'avancement de chaque projet, 
                                     que ce soit pour le nombre de places en crèche,
                                     les kilomètres de piste cyclable ou tout autre projet 
@@ -66,14 +66,14 @@ ob_start(); ?>
                     </div>
                 </div>
                 <div class="col-12 col-md-6 mb-5 mb-md-0 pl-0 pl-lg-3 pr-0">
-                    <div class="card card-principes shadow">
+                    <div class="card card-principes principes shadow">
                         <div class="card-body pb-5 pt-4">
                             <div class="pt-3 mb-4">
-                                <img src="assets/img/principes/principes-4.jpg" alt="" style="max-width:150px;"/>
+                                <img src="assets/img/principes/principes-4.jpg" alt=""/>
                             </div>
                             <div>
                                 <div class="w-900 font-25 mb-3">LE BAROMAIRE</div>
-                                <div class="content">
+                                <div class="principes content">
                                     peut s'intégrer facilement sur le site web de la commune 
                                 </div>
                             </div>
